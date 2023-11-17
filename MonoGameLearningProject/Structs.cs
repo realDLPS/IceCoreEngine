9d5437dc02c9cd8fa7872b07ecbab86906d79e24
DLPS <68390971+realDLPS@users.noreply.github.com>
2023-11-17 12:11:28 +0200

    Start moving VectorMath class to the engine
    To allow the engine to be easily cloned I am moving the class MonoGameVectorMath into this repo.

--- MonoGameLearningProject/Structs.cs
+++ MonoGameLearningProject/Structs.cs
@@ -2,7 +2,7 @@
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
-using MonoGameVectorMath;
+//using MonoGameVectorMath;
 using System;
 using System.Diagnostics;
 using System.Linq;
